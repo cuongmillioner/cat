a02553b7af8b61a18236354124c5e86717af1e17
DucTruong <36029725+Ozilll@users.noreply.github.com>
2018-04-12 03:01:20 +0700

    Update Login.js

--- My Project_CSE391/Sources/js/Login.js
+++ My Project_CSE391/Sources/js/Login.js
@@ -1,5 +1,32 @@
-function clickButton() {
-  alert("Updating");
+function testForm()
+{
+  var frm = document.forms["signin"];
+  //Test Email
+  var emai = frm["email"].value;
+  if(emai == "")
+  {
+    alert("Please enter your email!");
+    return false;
+  }
+  //Test password
+  var ps = frm["password"].value;
+  if(ps == "")
+  {
+    alert("Please enter your password!");
+    return false;
+  }
+  //Test Username
+  var uname = frm["username"].value;
+  if(uname == "")
+  {
+    alert("Please enter your username!");
+    return false;
+  }
+  var x = /[A-Z]@[a-z]+{2,10}$/;
+  uname = x;
+  if(uname == false)
+  {
+    alert("Wrong!");
+  }
+  return true;
 }
-var btnLogin=document.getElementById("btn-login");
-btnLogin.addEventListener("click",clickButton,false);
